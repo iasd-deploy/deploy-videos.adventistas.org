a1773b4155d8e8a97d601534b1c90975a08bfa42
Mizael Isaltino <mizael.isaltino@gmail.com>
2023-02-14 14:47:33 -0300

    Atualizasão plugins

--- app/pt/wp-content/plugins/wordpress-seo/storage/views/1/5c185edd6fe4c0f51f6db666a016356300b414af.php
+++
@@ -1,3 +0,0 @@
-<div class="row pt-5">
-	<InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($block['allowed_blocks'])); ?>" />
-</div><?php /**PATH /var/www/html/pt/wp-content/themes/pa-theme-sedes/Blocks/PARow/views/frontend.blade.php ENDPATH**/ ?>
--- app/pt/wp-content/plugins/wordpress-seo/storage/views/1/b192b099f5e574d5bb88d4dc1f1d4d05d9a54970.php
+++
@@ -1,7 +0,0 @@
-<?php echo get_header(); ?>
-
-
-<?php echo $__env->make('components.header.title', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('content'); ?>
-
-<?php echo get_footer(); ?><?php /**PATH /var/www/html/pt/wp-content/themes/pa-theme-videos/layouts/app.blade.php ENDPATH**/ ?>
